2e57cb918058578546803a1de0dcb2905bc1e105
Rafael Suárez Franco <44671951+RafaelSuarezFranco@users.noreply.github.com>
2019-03-07 09:48:14 +0100

    Add files via upload

---
+++ exercise-04.js
@@ -0,0 +1,6 @@
+
+            $(document).ready(function(){
+                $( "#header-width" ).html(  $("h1").width());
+                $( "#main-section-width" ).html($("div.section").width());
+
+        });
